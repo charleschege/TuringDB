747925abc255588fb52b2fadf174ad0a06ee0447
Charles Chege <charleschege@protonmail.ch>
2020-01-27 20:54:45 +0300

    Set up repo

--- ClientTester/src/main.rs
+++ ClientTester/src/main.rs
@@ -3,7 +3,7 @@ use async_std::{
     net::TcpStream,
 };
 use std::str;
-use turingfeeds_helpers::TuringCommand;
+use turingfeeds_helpers::{UnprivilegedTuringCommands, DocumentMethods};
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 const ADDRESS: &str = "127.0.0.1:43434";
@@ -14,10 +14,16 @@ const BUFFER_CAPACITY: usize = 64 * 1024;
 async fn main() -> Result<()> {
     let mut stream = TcpStream::connect(ADDRESS).await?;
     let mut buffer = [0; BUFFER_CAPACITY];
-    stream.write(&bincode::serialize::<TuringCommand>(&TuringCommand::CreateDatabase("db1".to_owned()))?).await?;
+
+    let data = DocumentMethods::new().await
+        .add_db("Turing1").await
+        .add_document("Doc1").await
+        .add_data("data".as_bytes().to_vec()).await;
+    
+    stream.write(&bincode::serialize::<UnprivilegedTuringCommands>(&UnprivilegedTuringCommands::CreateDocument(data))?).await?;
     let bytes_read = stream.read(&mut buffer).await?;
     
-    println!("{:?}", bincode::deserialize::<TuringCommand>(&buffer[..bytes_read]).unwrap());
+    println!("{:?}", bincode::deserialize::<custom_codes::DbOps>(&buffer[..bytes_read])?);
         
     Ok(())
 }
