4b272256cf61df2a1c02f025935e6204483df0bc
Charles Chege <charleschege@protonmail.ch>
2020-05-25 12:30:50 +0300

    move previous server to new repo

--- TuringFeeds/src/lib.rs
+++ TuringFeeds/src/lib.rs
@@ -7,8 +7,3 @@ mod loggers;
 //pub use engine::{REPO, READER, WRITER};
 pub use global::*;
 //pub use loggers::ErrorLogger;
-
-use tai64::TAI64N;
-use std::net::Shutdown;
-use custom_codes::DbOps;
-use anyhow::Result;
