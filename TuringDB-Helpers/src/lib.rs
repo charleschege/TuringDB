4d092ab83f3fc85d8a1fe4108be2ccd683d63787
Charles Chege <charleschege@protonmail.ch>
2020-06-29 10:16:43 +0300

    Add commands module

--- TuringDB-Helpers/src/lib.rs
+++ TuringDB-Helpers/src/lib.rs
@@ -2,13 +2,11 @@ use serde::{Serialize, Deserialize};
 
 mod repo;
 use repo::*;
-
 mod db;
 pub use db::*;
 mod document;
 pub use document::*;
 mod field;
 pub use field::*;
-
-const BUFFER_CAPACITY: usize = 64 * 1024; //16Kb
-const BUFFER_DATA_CAPACITY: usize = 1024 * 1024 * 16; // Db cannot hold data more than 16MB in size
+mod commands;
+pub use commands::*;
